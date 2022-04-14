9861c9e0a5166627899856f7192d744966086218
Riswan Mursidin <achmad.riswan@gmail.com>
2022-04-15 05:57:37 +0800

    Section Hero

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#f59e0b",
+        dark: "#78350f",
+        secondary: "#57534e",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
